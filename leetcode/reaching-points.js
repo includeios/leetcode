16731424fef81b577a20092a6ae90c8522ad21db
zhaoyi <zhaoyi@lianjia.com>
2019-04-17 10:30:01 +0800

    780. 到达终点

--- leetcode/reaching-points.js
+++ leetcode/reaching-points.js
@@ -6,19 +6,19 @@
  * @return {boolean}
  */
 var reachingPoints = function (sx, sy, tx, ty) {
-  while (tx > 0 && ty > 0){
-    if(tx === sx && ty === sy){
+  while (tx > 0 && ty > 0) {
+    if (tx === sx && ty === sy) {
       return true
-    } 
-    if(ty > tx){
-      const n =  Math.max(parseInt((ty - sy)/sx) ,1)
-      ty = ty - n*sx
-    } else {
-      const n = Math.max(parseInt((tx - sx) / sy),1)
-      tx = tx - n * sy
     }
-    
+    if (ty > tx) {
+      const n = Math.max(parseInt((ty - sy) / tx), 1)
+      ty = ty - n * tx
+    } else {
+      const n = Math.max(parseInt((tx - sx) / ty), 1)
+      tx = tx - n * ty
+    }
+
   }
 
-  return false
+  return false  
 };
