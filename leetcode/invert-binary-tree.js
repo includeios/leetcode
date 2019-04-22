7985a4b3b4783e5b63425870cba2f27e17afc160
zhaoyi <zhaoyi@lianjia.com>
2019-04-22 14:49:40 +0800

    翻转二叉树

---
+++ leetcode/invert-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  
+  const invertNode = (node)=>{
+    if (node) {
+      const tempNode = node.left
+      node.left = node.right
+      node.right = tempNode
+    }
+    invertNode(node.left)
+    invertNode(node.right)
+  }
+
+  invertNode(root)
+  return root
+  
+};
